f0b6ec0934a968a8195e1ffd6d24abce44cbd59f
Valerii Valeev <v@valeev.org>
2022-08-07 18:11:56 +0300

    Add output of command exit code

--- main.go
+++ main.go
@@ -2,29 +2,30 @@
 package main
 
 import (
-	"log"
+	"errors"
 	"flag"
 	"github.com/fsnotify/fsnotify"
-	"time"
-	"strings"
-	"os/exec"
+	"log"
 	"os"
+	"os/exec"
+	"strings"
+	"time"
 )
 
 func main() {
-    flag.Parse()
-    if len(flag.Args()) < 2 {
-	    log.Fatalf("Usage: onfse <path> <command>")
-    }
-    dir := flag.Arg(0)
-    log.Printf("Dir %v", dir)
+	flag.Parse()
+	if len(flag.Args()) < 2 {
+		log.Fatalf("Usage: onfse <path> <command>")
+	}
+	dir := flag.Arg(0)
+	log.Printf("Dir %v", dir)
 
-    cmd := flag.Args()[1:]
+	cmd := flag.Args()[1:]
 	log.Printf("Cmd %v", cmd)
 
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
-	 	log.Fatal(err)
+		log.Fatal(err)
 	}
 	defer watcher.Close()
 
@@ -39,7 +40,7 @@ func main() {
 					return
 				}
 				//log.Println("event:", event)
-				if event.Op & (fsnotify.Write | fsnotify.Create | fsnotify.Remove | fsnotify.Rename ) != 0 {
+				if event.Op&(fsnotify.Write|fsnotify.Create|fsnotify.Remove|fsnotify.Rename) != 0 {
 					//t := time.Now()
 					//if t.Sub(lastEventTime) < 100 * time.Millisecond {}
 					//log.Println("modified file:", event.Name, event.Op)
@@ -59,8 +60,8 @@ func main() {
 		log.Fatal(err)
 	}
 
-	waitDuration := 100*time.Millisecond
-	actionAfter  := time.Second / 2
+	waitDuration := 100 * time.Millisecond
+	actionAfter := time.Second / 2
 	idleLoopsMax := int(actionAfter / waitDuration)
 	//log.Printf("Idle Loops Max %v", idleLoopsMax)
 
@@ -80,10 +81,14 @@ func main() {
 					nidle += 1
 				}
 			} else {
+				headOpen := strings.Repeat("=", 48)
+				headClose := strings.Repeat("-", 48)
+				bodyEndError := strings.Repeat("+", 48)
+				bodyEndOk := strings.Repeat(".", 48)
 				if nevents > 0 && nidle >= idleLoopsMax {
-					println(strings.Repeat("=", 48))
+					println(headOpen)
 					log.Printf("RUN %v", cmd)
-					println(strings.Repeat("-", 48))
+					println(headClose)
 					var cmd_ *exec.Cmd
 					if len(cmd) == 1 {
 						cmd_ = exec.Command(cmd[0])
@@ -94,8 +99,13 @@ func main() {
 					cmd_.Stderr = os.Stderr
 					err := cmd_.Run()
 					if err != nil {
-						println(strings.Repeat("+", 48))
+						println(bodyEndError)
 						log.Printf("Command failed: %s", err)
+					} else {
+						var ee *exec.ExitError
+						errors.As(err, &ee)
+						log.Printf("Command returned %d", ee.ExitCode())
+						println(bodyEndOk)
 					}
 					nevents = 0
 					nidle = 0
