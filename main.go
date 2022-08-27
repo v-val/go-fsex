4bc7b736cb9885da78b43e4534bfc1b9dc6c0a27
Valerii Valeev <v@valeev.org>
2022-08-27 08:28:52 +0300

    Updated README and comments in code

--- main.go
+++ main.go
@@ -12,8 +12,10 @@ import (
 	"time"
 )
 
+// Type to store list of strings passed with repeating CLI flag
 type stringListFlag []string
 
+// Converter to string reqd to use type with flag package
 func (s *stringListFlag) String() string {
 	r := ""
 	for _, v := range *s {
@@ -24,31 +26,41 @@ func (s *stringListFlag) String() string {
 	}
 	return r
 }
+
+// Setter reqd to use type with flag package
 func (d *stringListFlag) Set(value string) error {
 	*d = append(*d, value)
 	return nil
 }
 
+// Main
 func main() {
-	var dirs stringListFlag
-	flag.Var(&dirs, "f", "File or dir to watch after")
+	// Get list of filesystem entities to watch from CLI
+	var fsEntities stringListFlag
+	flag.Var(&fsEntities, "f", "File or dir to watch after")
 	flag.Parse()
-	if len(dirs) < 1 || len(flag.Args()) < 1 {
+	// Check that at least one FS entity and at least one word command are passed
+	if len(fsEntities) < 1 || len(flag.Args()) < 1 {
 		log.Fatalf("Usage: onfse -f<path> [-f<path2> ...] <command>")
 	}
-	log.Printf("Dir %v", dirs)
+	log.Printf("Dir %v", fsEntities)
 
+	// Remaining CLi args treated as command
 	cmd := flag.Args()
 	log.Printf("Cmd %v", cmd)
 
+	// Create FS watcher
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer watcher.Close()
 
+	// Channel to notify about detected FS events
 	notifications := make(chan bool)
+	// Channel to pass halt instruction
 	done := make(chan bool)
+	// Coro that gets and filters events and passes notification to main thread
 	go func() {
 		//lastEventTime := time.Now()
 		for {
@@ -73,34 +85,44 @@ func main() {
 		}
 	}()
 
-	for _, f := range dirs {
+	// Pass FS entities to watcher
+	for _, f := range fsEntities {
 		err = watcher.Add(f)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
 
+	// TODO: make constants
 	waitDuration := 100 * time.Millisecond
 	actionAfter := time.Second / 2
 	idleLoopsMax := int(actionAfter / waitDuration)
 	//log.Printf("Idle Loops Max %v", idleLoopsMax)
 
+	// Number of events in last detected bunch
 	nevents := 0
+	// Number of idle loops since last detected event
 	nidle := 0
 	for {
 		select {
 		case _ = <-done:
+			// Got halt command
 			return
 		case _ = <-notifications:
+			// Event detected, do nothing
 			nevents += 1
 			nidle = 0
 		default:
+			// When no event detected we have two options
+			// * Execute command once actionAfter since last event
+			// * idle otherwise
 			if nevents == 0 || nidle < idleLoopsMax {
 				time.Sleep(waitDuration)
 				if nevents > 0 {
 					nidle += 1
 				}
 			} else {
+				// Print header, execute command
 				headOpen := strings.Repeat("=", 48)
 				headClose := strings.Repeat("-", 48)
 				bodyEndError := strings.Repeat("+", 48)
