f10a6e52cbe7e37ed39308509fc9184ecba60d2e
Valerii Valeev <v@valeev.org>
2023-12-05 17:22:23 +0400

    Logging wrapper - go-fsex-3 CLI flag "-q" suppressing command's output

---
+++ logging.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	L "log"
+)
+
+type logging_ struct {
+	Quietness incrementableInt
+}
+
+var loggingInstance = logging_{
+	Quietness: incrementableInt(0),
+}
+
+func SetQuietness(q incrementableInt) {
+	loggingInstance.Quietness = q
+}
+
+func Print(message any) {
+	if loggingInstance.Quietness <= 0 {
+		L.Println(message)
+	}
+}
+
+func Printf(format string, args ...any) {
+	if loggingInstance.Quietness <= 0 {
+		L.Printf(format, args...)
+	}
+}
+
+func Fatal(message any) {
+	L.Fatalln(message)
+}
+
+func Fatalf(format string, args ...any) {
+	L.Fatalf(format, args...)
+}
+
+func Panic(message any) {
+	L.Panicln(message)
+}
+
+func Panicf(format string, args ...any) {
+	L.Panicf(format, args...)
+}
