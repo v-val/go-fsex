b6943ea06c8ae81560b4976db1100b788fe850ba
Valerii Valeev <v@valeev.org>
2024-09-23 02:39:15 +0400

    [minor] code format improved - go-fsex-7 Filtering patterns are to follow ".gitignore" syntax

--- fsex.go
+++ fsex.go
@@ -18,32 +18,32 @@ type fsex struct {
 	// Operational vars
 }
 
-func (f *fsex) execCommand() {
+func (app *fsex) execCommand() {
 	// TODO: select line width respecting terminal properties
 	const hrWidth = 48
 	//headOpen := strings.Repeat("=", hrWidth) + "\n"
 	headClose := strings.Repeat("-", hrWidth)
 	bodyEndError := strings.Repeat("!", hrWidth) + "\n"
 	bodyEndOk := strings.Repeat(".", hrWidth) + "\n"
-	//hrBefore := headOpen + fmt.Sprintf("RUN %v\n", f.cmd) + headClose
+	//hrBefore := headOpen + fmt.Sprintf("RUN %v\n", app.cmd) + headClose
 	var cmd_ *exec.Cmd
-	if len(f.cmd) == 1 {
-		cmd_ = exec.Command(f.cmd[0])
+	if len(app.cmd) == 1 {
+		cmd_ = exec.Command(app.cmd[0])
 	} else {
-		cmd_ = exec.Command(f.cmd[0], f.cmd[1:]...)
+		cmd_ = exec.Command(app.cmd[0], app.cmd[1:]...)
 	}
-	if !f.flagSuppressStdout {
+	if !app.flagSuppressStdout {
 		cmd_.Stdout = os.Stdout
 	}
-	if !f.flagSuppressStderr {
+	if !app.flagSuppressStderr {
 		cmd_.Stderr = os.Stderr
 	}
-	if f.flagClearScreenOnChanges {
+	if app.flagClearScreenOnChanges {
 		Print("Clear the screen..")
 		screen.Clear()
 		screen.MoveTopLeft()
 	}
-	Printf("RUN %v\n"+headClose, f.cmd)
+	Printf("RUN %v\n"+headClose, app.cmd)
 	err := cmd_.Run()
 	if err != nil {
 		var ee *exec.ExitError
